544c75d5cb20ae4d2539cbd8d35475668cb6bb7e
diocagasburo <dondeodatomeliniedu@gmail.com>
2025-09-17 04:14:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,33 @@
 // File: sw.js
+const CACHE_NAME = 'drop-ticket-cache-v1';
+const urlsToCache = [
+    '/',
+    '/style.css',
+    '/index.html',
+    '/manifest.json',
+    '/anim.mp4',
+    '/refresh.json',
+    '/qr.png',
+    '/images/icon-192.png',
+    '/images/icon-512.png'
+];
 
-// Questo event listener è il minimo indispensabile per far sì che
-// il browser riconosca il sito come PWA installabile.
+// Installa il service worker e memorizza i file nella cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Intercetta le richieste di rete e restituisci i file dalla cache, se disponibili
 self.addEventListener('fetch', (event) => {
-  // Per ora non fa nulla, ma la sua presenza è fondamentale.
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
 });
